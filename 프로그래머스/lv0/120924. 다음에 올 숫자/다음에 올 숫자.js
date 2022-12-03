06be6782e5ea9bcd6c5e9e97541d3803b4a1efe2
Rieul <38723811+rieulp@users.noreply.github.com>
2022-12-03 18:35:00 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -0,0 +1,6 @@
+function solution(common) {
+    var answer = 0;
+    if(common[1]-common[0]===common[2]-common[1]) answer = common[common.length-1]+common[1]-common[0];
+    else answer = common[common.length-1]*(common[1]/common[0]);
+    return answer;
+}
