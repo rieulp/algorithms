4736019851c655a5a3d80e45fc05c1914b969e28
RaYeoung <38723811+rieulp@users.noreply.github.com>
2024-12-14 21:48:14 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,8 +1,8 @@
 function solution(s) {
     var answer;
     
-    const strings = s.split(' ');
-    const transformedStrings = strings.map((v)=>[...v].map((v, i)=>i%2===0 ? v.toUpperCase() : v.toLowerCase()).join(''))
+    const strings = s.toLowerCase().split(' ');
+    const transformedStrings = strings.map((v)=>[...v].map((v, i)=>i%2===0 ? v.toUpperCase() : v).join(''))
     answer = transformedStrings.join(' ')
     
     return answer;
