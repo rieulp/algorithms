34a8f280469b9cb14ca62aa57a7f58534d3949aa
RaYeoung <38723811+rieulp@users.noreply.github.com>
2024-12-14 21:37:04 +0900

    [level 1] Title: 체육복, Time: 3.51 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,47 +1,31 @@
 function solution(n, lost, reserve) {
     var answer = 0;
-    const students = Array(n+1).fill(1);
-    students[0] = 0;
-    for(s of reserve) students[s]++;
-    for(s of lost) students[s]--;
     
-    const reserveNum = reserve.length;
+    lost.sort();
+    reserve.sort();
     
-    function dfs(i) {
-         if(i===reserveNum){
-            answer = Math.max(answer, students.filter(val=>val>0).length)
-            return
-        }
-        
-        const reserveStudent = reserve[i];
-        
+    const students = Array(n).fill(1);
+    for(s of reserve) students[s-1]++;
+    for(s of lost) students[s-1]--;
+    
+    for(let i=0; i<reserve.length; i++) {
+        const reserveStudent = reserve[i] - 1;
         const left = reserveStudent - 1;
         const right = reserveStudent + 1;
         
-        if(students[reserveStudent]>1) {
-            if(left>0 && students[left]===0) {
-                students[left] ++;
-                students[reserveStudent]--;
-                dfs(i+1);
-                students[left]--;
-                students[reserveStudent]++;
-            }
+        if(students[reserveStudent]<2) continue;
         
-            if(right<=n && students[right]===0) {
-                students[right]++;
-                students[reserveStudent]--;
-                dfs(i+1);
-                students[right]--;
-                students[reserveStudent]++;
-            }
+        if(left>=0 && students[left]===0) {
+            students[left]++;
+            students[reserveStudent]--;
         }
-        
-       
-        
-        dfs(i+1)
+        else if(right<n && students[right]===0) {
+            students[right]++;
+            students[reserveStudent]--;
+        }
     }
-    
-    dfs(0)
+    console.log(students)
+    answer = students.filter(val=>val>0).length;
     
     return answer;
 }
