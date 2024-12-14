ff3847f0f18a7e7bb05406032650ed55fc482b09
RaYeoung <38723811+rieulp@users.noreply.github.com>
2024-12-15 00:55:55 +0900

    [level 3] Title: 입국심사, Time: 102.86 ms, Memory: 42.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238#) 
 
 ### 성능 요약
 
-메모리: 42.3 MB, 시간: 102.54 ms
+메모리: 42.3 MB, 시간: 102.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 15일 00:45:11
+2024년 12월 15일 00:55:53
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.js
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -1,8 +1,13 @@
 function solution(n, times) {
+    var answer = 0;
+    if(times.length>n) {
+        times.sort((a,b)=>a-b);
+        return times[n-1];
+    }
     
     let minTime = 1;
-    let maxTime = Math.max(...times) * n;
-    var answer = maxTime
+    let maxTime = Math.max(...times) * n
+    answer = maxTime;
     
     while(minTime<=maxTime) {
         const midTime = Math.floor((minTime+maxTime)/2);
